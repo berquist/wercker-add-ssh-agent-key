49e325ffbcb34b10226e1723d422c3dcfb4a43cb
Eric Berquist <berquist@isi.edu>
2019-08-20 18:23:49 -0400

    Don't use expect if a passphrase wasn't provided

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ KEY=$(eval echo "\$${WERCKER_ADD_SSH_AGENT_KEY_KEY}")
 # Check if SSH key is empty
 if [ "${KEY}" = "$" ] || [ -z "${KEY}" ]; then
     echo "SSH key is empty, no keys will be added."
+    exit
 fi
 
 # Store key to temp file
@@ -22,6 +23,7 @@ echo "${KEY}" > "${KEY_TEMP}"
 tail -n +2 "${WERCKER_STEP_ROOT}/start-agent.sh" >> ~/.bashrc
 
 # Check passphrase
+KEY_PASS=""
 if [ -n "${WERCKER_ADD_SSH_AGENT_KEY_PASSPHRASE}" ]; then
     KEY_PASS=$(eval "echo \$${WERCKER_ADD_SSH_AGENT_KEY_PASSPHRASE}")
 
@@ -38,12 +40,17 @@ if [ `command -v expect > /dev/null` ]; then
 fi
 
 # Add key to agent
-expect <<- EOF
+if [ -n "${WERCKER_ADD_SSH_AGENT_KEY_PASSPHRASE}" ]; then
+    expect <<- EOF
     spawn ssh-add "${KEY_TEMP}"
     expect "Enter passphrase"
     send "${KEY_PASS}\r"
     expect eof
 EOF
+else
+    # If a passphrase wasn't provided, there's no need for expect.
+    ssh-add "${KEY_TEMP}"
+fi
 
 # Unset variables
 KEY=""
